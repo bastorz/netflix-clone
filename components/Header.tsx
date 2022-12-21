25a4388d8ca154b153ef00de2e292f1baeac6e36
damian <damian@medanosol.es>
2022-12-21 16:12:24 +0000

    thumbnails now drop modal with trailer + data

---
+++ components/Header.tsx
@@ -0,0 +1,61 @@
+import {SearchIcon, BellIcon} from "@heroicons/react/solid"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import useAuth from "../hooks/useAuth";
+
+function Header() {
+  const { logout } = useAuth()
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0 ) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+    }
+    window.addEventListener("scroll", handleScroll)
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+  
+  return (
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className="object-contain cursor-pointer"
+        />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden w-6 h-6 sm:inline"/>
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="hidden w-6 h-6 sm:inline"/>
+        <p className="hidden lg:inline"></p>
+        {/* <Link href="/account"> */}
+        <img
+          onClick={logout}
+          src="https://rb.gy/g1pwyx"
+          width={100}
+          height={100}
+          className="w-6 h-6 rounded cursor-pointer"
+        />
+        {/* </Link> */}
+      </div>
+    </header>
+  )
+}
+
+export default Header
