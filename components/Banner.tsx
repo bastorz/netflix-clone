8d13e5273045922640b6963d6d3ace5cbfd33e28
bastorz <damianalcalayy@gmail.com>
2023-02-23 03:00:11 +0100

    installed codemod

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,57 +1,60 @@
-import { Movie } from "../typings"
-import Image from 'next/image'
-import { useState, useEffect } from "react"
-import { baseUrl } from "../constants/movie"
-import {FaPlay} from "react-icons/fa"
-import { InformationCircleIcon } from "@heroicons/react/solid"
-import { useRecoilState } from "recoil"
-import { modalState, movieState } from "../atoms/modalAtom"
+import { Movie } from "../typings";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { baseUrl } from "../constants/movie";
+import { FaPlay } from "react-icons/fa";
+import { InformationCircleIcon } from "@heroicons/react/solid";
+import { useRecoilState } from "recoil";
+import { modalState, movieState } from "../atoms/modalAtom";
 
 interface Props {
-    netflixOriginals: Movie[]
+  netflixOriginals: Movie[];
 }
 
+const Banner = ({ netflixOriginals }: Props) => {
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const [showModal, setShowModal] = useRecoilState(modalState);
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState);
 
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    );
+  }, [netflixOriginals]);
 
-
-const Banner = ({netflixOriginals}: Props ) => {
-  const [movie, setMovie] = useState<Movie | null >(null)
-  const [showModal, setShowModal] = useRecoilState(modalState)
-  const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
-
-    useEffect(() => {
-        setMovie(
-            netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
-        )
-    }, [netflixOriginals])
-
-  return ( 
+  return (
     <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
-        <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
-            <Image src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`} alt={""}
-            layout="fill" objectFit="cover"/>
-        </div>
+      <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          alt={""}
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
 
-        <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
-            {movie?.title || movie?.name || movie?.original_name}
-        </h1>
-        <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
-            {movie?.overview}
-        </p>
-        <div className="flex space-x-3 ">
-            <button className="text-black bg-white bannerButton"><FaPlay className="w-4 h-4 text-black md:h-7 md:w-7"/> Play</button>
-            <button
-                className="bannerButton bg-[gray]/70"
-                onClick={() => {
-                setCurrentMovie(movie)
-                 setShowModal(true)
-                }}
-            >
+      <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+        {movie?.overview}
+      </p>
+      <div className="flex space-x-3 ">
+        <button className="text-black bg-white bannerButton">
+          <FaPlay className="w-4 h-4 text-black md:h-7 md:w-7" /> Play
+        </button>
+        <button
+          className="bannerButton bg-[gray]/70"
+          onClick={() => {
+            setCurrentMovie(movie);
+            setShowModal(true);
+          }}
+        >
           <InformationCircleIcon className="w-5 h-5 md:h-8 md:w-8" /> More Info
         </button>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { useRecoilValue } from 'recoil'
-import { modalState } from '../atoms/modalAtom'
-import Banner from '../components/Banner'
-import Header from '../components/Header'
-import Modal from '../components/Modal'
-import Row from '../components/Row'
-import useAuth from '../hooks/useAuth'
-import { Movie } from '../typings'
-import requests from '../utils/requests'
+import Head from "next/head";
+import Image from "next/image";
+import { useRecoilValue } from "recoil";
+import { modalState } from "../atoms/modalAtom";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import Modal from "../components/Modal";
+import Row from "../components/Row";
+import useAuth from "../hooks/useAuth";
+import { Movie } from "../typings";
+import requests from "../utils/requests";
 
 interface Props {
-  netflixOriginals: Movie[]
-  trendingNow: Movie[]
-  topRated: Movie[]
-  actionMovies: Movie[]
-  comedyMovies: Movie[]
-  horrorMovies: Movie[]
-  romanceMovies: Movie[]
-  documentaries: Movie[]
+  netflixOriginals: Movie[];
+  trendingNow: Movie[];
+  topRated: Movie[];
+  actionMovies: Movie[];
+  comedyMovies: Movie[];
+  horrorMovies: Movie[];
+  romanceMovies: Movie[];
+  documentaries: Movie[];
 }
 
-const Home = ({   netflixOriginals,
+const Home = ({
+  netflixOriginals,
   actionMovies,
   comedyMovies,
   documentaries,
@@ -29,12 +30,11 @@ const Home = ({   netflixOriginals,
   romanceMovies,
   topRated,
   trendingNow,
- }: Props) => {
+}: Props) => {
+  const { loading } = useAuth();
+  const showModal = useRecoilValue(modalState);
 
-  const { loading } = useAuth()
-  const showModal = useRecoilValue(modalState)
-
-  if (loading) return null
+  if (loading) return null;
 
   return (
     <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
@@ -42,9 +42,9 @@ const Home = ({   netflixOriginals,
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header/>
-      <main className='relative pb-24 pl-4 lg:space-y-24 lg:pl-16'>
-        <Banner netflixOriginals={netflixOriginals}/>
+      <Header />
+      <main className="relative pb-24 pl-4 lg:space-y-24 lg:pl-16">
+        <Banner netflixOriginals={netflixOriginals} />
         <section className="md:space-y-24">
           <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
@@ -56,12 +56,12 @@ const Home = ({   netflixOriginals,
           <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
-        {showModal && <Modal/>}
+      {showModal && <Modal />}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 export const getServerSideProps = async () => {
   const [
@@ -82,7 +82,7 @@ export const getServerSideProps = async () => {
     fetch(requests.fetchHorrorMovies).then((res) => res.json()),
     fetch(requests.fetchRomanceMovies).then((res) => res.json()),
     fetch(requests.fetchDocumentaries).then((res) => res.json()),
-  ])
+  ]);
 
   return {
     props: {
@@ -95,5 +95,5 @@ export const getServerSideProps = async () => {
       romanceMovies: romanceMovies.results,
       documentaries: documentaries.results,
     },
-  }
-}
+  };
+};
